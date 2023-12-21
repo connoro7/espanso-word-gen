c52a73ed7b6bc037ae6d49888fe95c0a1387f19d
Connor Dillon <connor@connordillon.dev>
2023-12-20 17:01:27 -0800

    Update test files

--- tests/functionaltests.py
+++ tests/functionaltests.py
@@ -12,5 +12,47 @@ class TestMain(unittest.TestCase):
     def tearDown(self):
         pass
 
-    def test_main(self):
+    def test_can_handle_txt_file(self):
+        self.assertEqual(main.main(DEBUG=True), None)
+
+    def test_can_handle_md_file(self):
+        self.assertEqual(main.main(DEBUG=True), None)
+
+    def test_can_handle_sh_file(self):
+        self.assertEqual(main.main(DEBUG=True), None)
+
+    def test_can_handle_yaml_file(self):
+        self.assertEqual(main.main(DEBUG=True), None)
+
+    def test_can_handle_json_file(self):
+        self.assertEqual(main.main(DEBUG=True), None)
+
+    def test_can_handle_dot_file(self):
+        self.assertEqual(main.main(DEBUG=True), None)
+
+    def test_can_handle_c_file(self):
+        self.assertEqual(main.main(DEBUG=True), None)
+
+    def test_can_handle_cpp_file(self):
+        self.assertEqual(main.main(DEBUG=True), None)
+
+    def test_can_handle_java_file(self):
+        self.assertEqual(main.main(DEBUG=True), None)
+
+    def test_can_handle_docker_file(self):
+        self.assertEqual(main.main(DEBUG=True), None)
+
+    def test_can_handle_js_file(self):
+        self.assertEqual(main.main(DEBUG=True), None)
+
+    def test_can_handle_ts_file(self):
+        self.assertEqual(main.main(DEBUG=True), None)
+
+    def test_can_handle_lua_file(self):
+        self.assertEqual(main.main(DEBUG=True), None)
+
+    def test_can_handle_pdf_file(self):
+        self.assertEqual(main.main(DEBUG=True), None)
+
+    def test_can_handle_docx_file(self):
         self.assertEqual(main.main(DEBUG=True), None)
